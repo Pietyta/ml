62b83857cf5c76e78518e252c740266907753a19
pietyta <takoua.chaabane@gmail.com>
2019-02-04 21:36:16 +0100

    First commit

---
+++ svm/svm_1.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn import svm
+
+digits = datasets.load_digits()
+
+clf = svm.SVC(gamma = 0.001 , C=100)
+
+ns = print(len(digits.data))
+
+x,y = digits.data[:-10] , digits.target[:-10]
+
+clf.fit(x,y)
+
+print('Predection:' , clf.predict(digits.data[-1].reshape(1, -1)))
+plt.imshow(digits.images[-1], cmap= plt.cm.gray_r, interpolation="nearest")
+
+plt.show()
+            
+
